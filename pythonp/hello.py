8a04d1b6800f6ead8584de8be2174f3ee9a001e2
MutisyaVuta <nicholas.mutisya77@gmail.com>
2024-08-30 04:14:11 +0300

    str

--- pythonp/hello.py
+++ pythonp/hello.py
@@ -1,1 +1,17 @@
-print('Hello world')
+#print('Hello world')
+
+# name = input("What is your name? " )
+# print("Hello " +  name)
+
+
+# birth_year = input("Enter your birth year:  ")
+# age = 2024 - int(birth_year)
+# print(age)
+
+# first_number = float(input("Enter first number: "))
+# second_number = float(input("Enter second number: "))
+# sum = first_number + second_number
+
+# print("The sum is: " + str(sum))
+
+
