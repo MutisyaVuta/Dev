aa9ef22837e071f2880df24aedcf2040efb6a390
MutisyaVuta <nicholas.mutisya77@gmail.com>
2024-08-30 05:41:31 +0300

    done

--- pythonp/hello.py
+++ pythonp/hello.py
@@ -15,3 +15,25 @@
 # print("The sum is: " + str(sum))
 
 
+#strmethods
+course = 'Python for Beginners'
+
+# print(course.upper())
+# print(course.lower())
+# print(course.capitalize())
+# print(course)
+# print(course.find ('Python'))
+# print(course.replace('for', '4' ))
+
+# print('Python' in course)
+
+#Arithmetic operators in python
+
+
+x = 20
+x += 3
+print(x)
+
+
+#weight converter
+weight = input("weight: ")
