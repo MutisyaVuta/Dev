95a2c94d531eafa71156e83b91865b7ce4a7bf92
MutisyaVuta <nicholas.mutisya77@gmail.com>
2024-08-30 07:13:46 +0300

    lists

---
+++ pythonp/app.py
@@ -0,0 +1,14 @@
+# names = ["John", "Kim", "Nick", "Kamaa", "Esther"]
+# names[1] = 'Cel'
+# print(names[0:3])
+# print(names)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# for number in numbers:
+#     print(number* '*')
+
+#rangefunction
+
+for item in range(5,20,3):
+    print(item)
