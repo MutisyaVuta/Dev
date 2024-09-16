5f0eab9b99070ca616e438d8463da9419a8213a2
Nick <nicholas.mutisya77@gmail.com>
2024-09-16 09:44:41 +0300

    refresher

--- pythonp/app.py
+++ pythonp/app.py
@@ -1,14 +1,24 @@
-# names = ["John", "Kim", "Nick", "Kamaa", "Esther"]
-# names[1] = 'Cel'
-# print(names[0:3])
-# print(names)
+# # names = ["John", "Kim", "Nick", "Kamaa", "Esther"]
+# # names[1] = 'Cel'
+# # print(names[0:3])
+# # print(names)
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-# for number in numbers:
-#     print(number* '*')
+# # for number in numbers:
+# #     print(number* '*')
 
-#rangefunction
+# #rangefunction
 
-for item in range(5,20,3):
-    print(item)
+# for item in range(5,20,3):
+#     print(item)
+
+# Strings
+# print('Hello World')
+# fav_game = "football"
+# print(f"My favorite game is {fav_game}")
+# email = "nicholas.mutisya77@gmail.com"
+# print(f"Your email is {email}")
+
+players = 30
+print(f"Kasarani homeboyz has {players} registered for 2024/2025 season")
