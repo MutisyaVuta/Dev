12fe31e5568b4f03b8489e08a339e75587149660
Nick <nicholas.mutisya77@gmail.com>
2024-09-16 10:04:23 +0300

    boolen

--- pythonp/app.py
+++ pythonp/app.py
@@ -20,5 +20,28 @@
 # email = "nicholas.mutisya77@gmail.com"
 # print(f"Your email is {email}")
 
-players = 30
-print(f"Kasarani homeboyz has {players} registered for 2024/2025 season")
+# Integers
+# players = 30
+# print(f"Kasarani homeboyz has {players} registered for 2024/2025 season")
+
+# # float
+# distance = 5.5
+# print(f"distance covered is {distance}")
+
+# is_student = False
+# if is_student:
+#     print("You are a student")
+# else:
+#     print("You are NOT a student")
+
+# is_tranfer_window_open = True
+# if is_tranfer_window_open:
+#     print("You can request new player")
+# else:
+#     print("Transfer window closed")
+
+is_married = False
+if is_married:
+    print("Not available for dating")
+else:
+    print("Ready to mingle")
