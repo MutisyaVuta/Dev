403711dc64faf45a2b24037f292940567d4e01d5
Nick <nicholas.mutisya77@gmail.com>
2024-09-16 11:29:30 +0300

    accepting user input

--- pythonp/convo.py
+++ pythonp/convo.py
@@ -9,16 +9,34 @@
 # distance = int(distance)
 # print(distance)
 
-#input()= A function that prompts the user to enter a data 
-#RETURNS the entered data as a string
+# input()= A function that prompts the user to enter a data 
+# RETURNS the entered data as a string
 
-name = input("What is your name?: ")
-age = input("How old are you?: ")
+# name = input("What is your name?: ")
+# age = int(input("How old are you?: "))
 
-age = int(age)
 
-age = age + 1
 
-print(f"My name is {name}")
-print("HAPPY BIRTHDAY")
-print(f"You are {age} years old")
+# age = age + 1
+
+# print(f"My name is {name}")
+# print("HAPPY BIRTHDAY")
+# print(f"You are {age} years old")
+
+# AREA OF A RECTANGLE
+
+# Width = int(input("Enter the Width: " ))
+# Length = int(input("Enter the Length: "))
+
+# area = Length*Width
+
+# print(f"The area of a rectangle is {area} cm ")
+
+#shopping cart program
+item = input("What item would you like to buy?: ")
+price = float(input ("What is the price?: "))
+quantity = int(input("How many items would you like?: "))
+
+total_price = price*quantity
+
+print(f"The total is {total_price} dollars")
