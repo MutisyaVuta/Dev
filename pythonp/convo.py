f465744eb8d70ce2320839e756615f10391a8a23
Nick <nicholas.mutisya77@gmail.com>
2024-09-16 10:57:08 +0300

    inputs

---
+++ pythonp/convo.py
@@ -0,0 +1,24 @@
+# name = "Jane"
+# distance =30.55
+# inti = 20
+# is_married = True
+
+# # print(type(distance))
+
+# # print(float(int))
+# distance = int(distance)
+# print(distance)
+
+#input()= A function that prompts the user to enter a data 
+#RETURNS the entered data as a string
+
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+
+age = int(age)
+
+age = age + 1
+
+print(f"My name is {name}")
+print("HAPPY BIRTHDAY")
+print(f"You are {age} years old")
